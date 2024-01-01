d3c24c408f371281a5ac265441d60cbce72fcc93
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2024-01-01 19:49:34 +0300

    using puppet to configure client

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,15 @@
-#!/usr/bin/env bash
-# using puppet to make changes to our config file
-
+#!/usr/bin/env pup
+# Configures SSH Replacing strings
 file { '/etc/ssh/ssh_config':
-	ensure => present.
-content =>"
-	#SSH client configuration
-	host *
-	IdentityFile ~/.ssh/school
-	PasswordAuthentication no
-	",
+  ensure => present,
+}
+file_line { 'Replace a line to /etc/ssh/ssh_config':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    IdentityFile ~/.ssh/school',
+  match => '^IdentityFile.*$',
+}
+file_line { 'Append a line to /etc/ssh/ssh_config':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    PasswordAuthentication no',
+  match => '^PasswordAuthentication.*$',
 }
