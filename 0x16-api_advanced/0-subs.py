203b5e0940817b29700d1ec39dffa38aa2d8e234
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2024-02-13 19:48:19 +0300

    task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,30 +1,19 @@
-#!/usr/python3
+#!/usr/bin/python3
+"""returns the number of subscribers"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    function that returns the number of subscribers
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    """Returns the number of subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                      'AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/58.0.3029.110 Safari/537.3'
+        "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) \
+        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 \
+            Mobile Safari/537.36"
     }
-
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        if 'data' in data and 'subscribers' in data['data']:
-            return data['data']['subscribers']
-        else:
-            print("Subreddit data not found")
-            return 0
-    elif response.status_code == 404:
-        print("Subreddit not found")
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    else:
-        print("Error occurred while fetching subreddit data")
-        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
